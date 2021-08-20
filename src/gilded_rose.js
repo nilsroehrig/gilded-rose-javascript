973989acbd93add2525b10e6a54ac0e20c5aebca
Nils Röhrig <1717688+nilsroehrig@users.noreply.github.com>
2021-08-20 12:10:45 +0200

    convert syntax, add jest, remove jasmine

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -1,20 +1,25 @@
-function Item(name, sell_in, quality) {
-  this.name = name;
-  this.sell_in = sell_in;
-  this.quality = quality;
+class Item {
+  constructor(name, sell_in, quality) {
+    this.name = name
+    this.sell_in = sell_in
+    this.quality = quality
+  }
 }
 
-var items = []
+const items = []
 
-items.push(new Item('+5 Dexterity Vest', 10, 20));
-items.push(new Item('Aged Brie', 2, 0));
-items.push(new Item('Elixir of the Mongoose', 5, 7));
-items.push(new Item('Sulfuras, Hand of Ragnaros', 0, 80));
-items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20));
-items.push(new Item('Conjured Mana Cake', 3, 6));
+items.push(new Item('+5 Dexterity Vest', 10, 20))
+items.push(new Item('Aged Brie', 2, 0))
+items.push(new Item('Elixir of the Mongoose', 5, 7))
+items.push(new Item('Sulfuras, Hand of Ragnaros', 0, 80))
+items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20))
+items.push(new Item('Conjured Mana Cake', 3, 6))
+// --------------------------------------------------------------------------------------------
+// Do not touch above, because Goblin!
+// --------------------------------------------------------------------------------------------
 
-function update_quality() {
-  for (var i = 0; i < items.length; i++) {
+function update_quality (items = []) {
+  for (let i = 0; i < items.length; i++) {
     if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
       if (items[i].quality > 0) {
         if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
@@ -39,7 +44,7 @@ function update_quality() {
       }
     }
     if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-      items[i].sell_in = items[i].sell_in - 1;
+      items[i].sell_in = items[i].sell_in - 1
     }
     if (items[i].sell_in < 0) {
       if (items[i].name != 'Aged Brie') {
@@ -60,3 +65,5 @@ function update_quality() {
     }
   }
 }
+
+module.exports = { update_quality }
---
+++ src/gilded_rose.test.js
@@ -0,0 +1,6 @@
+const { update_quality } = require('./gilded_rose')
+
+it('works', () => {
+  console.log('works')
+  throw 'Error'
+})
